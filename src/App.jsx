030dce8b68602fcefa9bcf383270eca51920b046
Thomas.Baumeister <thomasbaumeister3@gmail.com>
2023-08-07 17:23:07 -0500

    finally got the page to work properly by changing my routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import DisplayPlayers from './components/DisplayAllPlayers'
 import './App.css'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import PlayerDetailsPage from './components/PlayerDetail.jsx';
 
 const API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/2306-fsa-et-web-ft-sf/players`
@@ -16,7 +16,7 @@ function App() {
         const allPlayerList = data.data.players
         console.log(allPlayerList)
         setPlayerList(allPlayerList)
-       
+
 
       } catch (err) {
         console.log(err)
@@ -30,16 +30,18 @@ function App() {
   };
 
   return (
-    <Router>
-      {/* Route for displaying the list of players */}
-      <Route exact path="/">
-        <DisplayPlayers playerList={playerList} onPlayerDetailsClick={handlePlayerDetailsClick} />
-      </Route>
-      {/* Route for displaying individual player details */}
-      <Route path="/player/:id">
-        <PlayerDetailsPage playerList={playerList} />
-      </Route>
-    </Router>
+    <>
+      <Routes>
+        {/* Route for displaying the list of players */}
+        <Route path="/player" element={<DisplayPlayers playerList={playerList} onPlayerDetailsClick={handlePlayerDetailsClick} />} />
+
+        {/* Route for displaying individual player details */}
+        <Route path="/player/:id" element={<PlayerDetailsPage playerList={playerList} />} />
+
+      </Routes>
+
+    </>
+
   );
 }
 export default App
